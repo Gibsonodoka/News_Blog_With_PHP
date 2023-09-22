1cd4cbf3626a9ad058f46c3d63f518260f5795cc
Gibsonodoka <vellagibbs@gmail.com>
2023-09-22 14:26:35 +0100

    added bootstrap

--- admin_header.php
+++ admin_header.php
@@ -5,20 +5,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard | Welcome</title>
     <link rel="stylesheet" href="public/css/admin-style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script> 
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script> 
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+     
 </head>
 <body>
 
 <header>
-    <div class="logo">
+    <div class="admin-logo">
         <img src="/Blog/public/images/job.png" alt="Your Logo">
     </div>
-    <div class="admin-nav">
-        <a href="../index.php" class="view-site-button">View Site</a>
+    <div class="admin-navigation">
+        <a href="../index.php" class="admin-view-site-button">View Site</a>
         <div class="admin-profile">
             <img src="/Blog/public/images/avatar.png" alt="Admin Profile"> <!-- Replace with the path to your dummy avatar -->
-            <div class="dropdown">
-                <button class="profile-dropdown-button"><?php echo $adminName; ?></button>
-                <div class="dropdown-content">
+            <div class="admin-dropdown">
+                <button class="admin-profile-dropdown-button"><?php echo $adminName; ?></button>
+                <div class="admin-dropdown-content">
                     <a href="#">Profile</a>
                     <a href="logout.php">Logout</a>
                 </div>
--- auth/manage_users.php
+++ auth/manage_users.php
@@ -49,12 +49,6 @@ $totalPages = ceil($totalUsers / $usersPerPage);
 <html>
 <head>
     <title>Manage Users</title>
-    <link rel="stylesheet" href="../public/css/admin-style.css">
-    <!-- Include Bootstrap CSS and JS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body>
     <div class="content">
@@ -105,26 +99,25 @@ $totalPages = ceil($totalUsers / $usersPerPage);
             ?>
         </div>
 
-        <!-- Modal for confirming user deletion -->
         <div class="modal fade" id="confirmDeleteModal" tabindex="-1" role="dialog" aria-labelledby="confirmDeleteModalLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="confirmDeleteModalLabel">Confirm Deletion</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <p>Are you sure you want to delete user: <span id="deleteUserName"></span>?</p>
-                    </div>
-                    <div class="modal-footer">
-                        <a href="#" class="btn btn-danger" id="confirmDeleteButton">Yes</a>
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">No</button>
-                    </div>
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="confirmDeleteModalLabel">Confirm Deletion</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p>Are you sure you want to delete user: <span id="deleteUserName"></span>?</p>
+                </div>
+                <div class="modal-footer">
+                    <a href="#" class="btn btn-danger" id="confirmDeleteButton">Yes</a>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">No</button>
                 </div>
             </div>
         </div>
+    </div>
 
         <!-- JavaScript to handle the Delete User link click -->
         <script>
