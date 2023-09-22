e74b551fe06c259c122b4eac7c9b53074b7b3a19
Gibsonodoka <vellagibbs@gmail.com>
2023-09-22 11:10:06 +0100

    Added Pag to User_management

--- auth/manage_users.php
+++ auth/manage_users.php
@@ -20,6 +20,29 @@ $adminName = $statement->fetchColumn();
 // Include the admin header and navigation
 require '../admin_header.php'; // Adjust the path as needed
 require '../dash_nav.php';
+
+// Pagination variables
+$usersPerPage = 10; // Adjust as needed
+$page = isset($_GET['page']) ? $_GET['page'] : 1; // Get the current page from the URL
+
+// Calculate the OFFSET for SQL query
+$offset = ($page - 1) * $usersPerPage;
+
+// Fetch and display the list of users with pagination
+$query = "SELECT id, username, email, role, status, created_at FROM users LIMIT :limit OFFSET :offset";
+$statement = $conn->prepare($query);
+$statement->bindValue(':limit', $usersPerPage, PDO::PARAM_INT);
+$statement->bindValue(':offset', $offset, PDO::PARAM_INT);
+$statement->execute();
+$user_data = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+// Calculate the total number of users
+$totalUsersQuery = "SELECT COUNT(*) FROM users";
+$totalUsersStatement = $conn->query($totalUsersQuery);
+$totalUsers = $totalUsersStatement->fetchColumn();
+
+// Calculate the total number of pages
+$totalPages = ceil($totalUsers / $usersPerPage);
 ?>
 
 <!DOCTYPE html>
@@ -33,51 +56,49 @@ require '../dash_nav.php';
         <h2>Manage Users</h2>
         <a href="add_user.php" class="add-user-button">Add User</a> <!-- Link to add a new user -->
    
+        <!-- Display user data in a table -->
+        <table>
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Username</th>
+                    <th>Email</th>
+                    <th>Role</th>
+                    <th>Status</th>
+                    <th>Date Registered</th>
+                    <th>Options</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                foreach ($user_data as $user) {
+                    echo "<tr>";
+                    echo "<td>{$user['id']}</td>";
+                    echo "<td>{$user['username']}</td>";
+                    echo "<td>{$user['email']}</td>";
+                    echo "<td>{$user['role']}</td>";
+                    echo "<td>{$user['status']}</td>";
+                    echo "<td>{$user['created_at']}</td>";
+                    echo "<td>
+                            <a href='edit_user.php?id={$user['id']}' class='edit-user-button'>Edit User</a>|  
+                            <a href='delete_user.php?id={$user['id']}' class='delete-user-button'>Delete User</a>
+                          </td>";
+                    echo "</tr>";
+                }
+                ?>
+            </tbody>
+        </table>
 
-    <!-- Display user data in a table -->
-    <table>
-        <thead>
-            <tr>
-                <th>Id</th>
-                <th>Username</th>
-                <th>Email</th>
-                <th>Role</th>
-                <th>Status</th>
-                <th>Date Registered</th>
-                <th>Options</th>
-            </tr>
-        </thead>
-        <tbody>
+        <!-- Pagination links -->
+        <div class="pagination">
             <?php
-            // Fetch and display user data here
-            $query = "SELECT id, username, email, role, status, created_at FROM users";
-            $statement = $conn->prepare($query);
-            $statement->execute();
-            $user_data = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-            foreach ($user_data as $user) {
-                echo "<tr>";
-                echo "<td>{$user['id']}</td>";
-                echo "<td>{$user['username']}</td>";
-                echo "<td>{$user['email']}</td>";
-                echo "<td>{$user['role']}</td>";
-                echo "<td>{$user['status']}</td>";
-                echo "<td>{$user['created_at']}</td>";
-                echo "<td>
-                        <a href='edit_user.php?id={$user['id']}' class='edit-user-button'>Edit User</a>|  
-                        <a href='delete_user.php?id={$user['id']}' class='delete-user-button'>Delete User</a>
-                      </td>";
-                echo "</tr>";
+            // Output pagination links
+            for ($i = 1; $i <= $totalPages; $i++) {
+                $activeClass = ($i == $page) ? 'active' : '';
+                echo "<a href='manage_users.php?page={$i}' class='pagination-link {$activeClass}'>{$i}</a>";
             }
             ?>
-        </tbody>
-    </table>
-
-    <!-- Add pagination here if you have many users -->
-
-    <!-- Add search and filter options if needed -->
-
-    <!-- Add JavaScript for any client-side functionality -->
-
+        </div>
+    </div>
 </body>
 </html>
